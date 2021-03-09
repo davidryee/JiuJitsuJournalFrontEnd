e585eecbebe0de513f6ba82b4e046b5f07e74f08
davidryee <davidryee@gmail.com>
2021-03-08 21:47:11 -0800

    All screens have side nav bar. Opponents screen created

---
+++ src/app/opponents-screen/opponents-screen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-opponents-screen',
+  templateUrl: './opponents-screen.component.html',
+  styleUrls: ['./opponents-screen.component.css']
+})
+export class OpponentsScreenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
